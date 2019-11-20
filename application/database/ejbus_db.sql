02910c1edb6bb2d76b2da40695080ee3f0d3baba
Tristan Rosales <tristanrosales0@gmail.com>
2019-11-20 22:33:04 +0800

    Added database file.

---
+++ application/database/ejbus_db.sql
@@ -0,0 +1,193 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 20, 2019 at 03:31 PM
+-- Server version: 10.1.37-MariaDB
+-- PHP Version: 7.3.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `ejbus_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bus`
+--
+
+CREATE TABLE `bus` (
+  `bus_id` int(255) NOT NULL,
+  `bus_name` varchar(100) NOT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `active_flag` enum('Y','N') NOT NULL DEFAULT 'Y'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `bus`
+--
+
+INSERT INTO `bus` (`bus_id`, `bus_name`, `created_date`, `active_flag`) VALUES
+(1, 'ABLiner', '2019-11-17 18:26:18', 'Y'),
+(2, 'Saulog', '2019-11-17 18:26:31', 'Y');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `param_roles`
+--
+
+CREATE TABLE `param_roles` (
+  `role_code` varchar(255) NOT NULL,
+  `role_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `param_roles`
+--
+
+INSERT INTO `param_roles` (`role_code`, `role_name`) VALUES
+('ADMIN', 'Administrator');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE `reservation` (
+  `reservation_id` int(255) NOT NULL,
+  `reservation_origin` varchar(100) NOT NULL,
+  `reservation_destination` varchar(100) NOT NULL,
+  `reservation_date` varchar(100) NOT NULL,
+  `reservation_time` varchar(100) NOT NULL,
+  `reservation_bus` mediumint(8) NOT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reservation`
+--
+
+INSERT INTO `reservation` (`reservation_id`, `reservation_origin`, `reservation_destination`, `reservation_date`, `reservation_time`, `reservation_bus`, `created_date`) VALUES
+(1, 'Manila', 'Makati', '11/18/2019', '12:00 PM', 2, '2019-11-17 18:53:03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_id` int(255) NOT NULL,
+  `username` varchar(100) DEFAULT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `created_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `active_flag` enum('Y','N') NOT NULL DEFAULT 'Y'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_id`, `username`, `email`, `password`, `created_date`, `active_flag`) VALUES
+(1, 'admin_ejbus', 'admin.ejbus@gmail.com', '$2y$10$z1GkaABaM2KgG5.pJgQ7ZOPu88dfrRwLxju5csJ4o4F6M1Wrryygq', '2019-11-17 01:00:20', 'Y');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_roles`
+--
+
+CREATE TABLE `user_roles` (
+  `user_id` mediumint(8) NOT NULL,
+  `role_code` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+INSERT INTO `user_roles` (`user_id`, `role_code`) VALUES
+(1, 'ADMIN');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `bus`
+--
+ALTER TABLE `bus`
+  ADD PRIMARY KEY (`bus_id`),
+  ADD UNIQUE KEY `bus_id_UNIQUE` (`bus_id`),
+  ADD UNIQUE KEY `bus_name_UNIQUE` (`bus_name`);
+
+--
+-- Indexes for table `param_roles`
+--
+ALTER TABLE `param_roles`
+  ADD PRIMARY KEY (`role_code`),
+  ADD UNIQUE KEY `role_code_UNIQUE` (`role_code`);
+
+--
+-- Indexes for table `reservation`
+--
+ALTER TABLE `reservation`
+  ADD PRIMARY KEY (`reservation_id`),
+  ADD UNIQUE KEY `reservation_id_UNIQUE` (`reservation_id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_id`),
+  ADD UNIQUE KEY `id_UNIQUE` (`user_id`),
+  ADD UNIQUE KEY `email_UNIQUE` (`email`),
+  ADD UNIQUE KEY `username_UNIQUE` (`username`);
+
+--
+-- Indexes for table `user_roles`
+--
+ALTER TABLE `user_roles`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bus`
+--
+ALTER TABLE `bus`
+  MODIFY `bus_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `reservation`
+--
+ALTER TABLE `reservation`
+  MODIFY `reservation_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
